f0801aeca547919c5cabdbde589eddcb19d1d8e9
om0rgado <115956497+om0rgado@users.noreply.github.com>
2022-12-11 22:25:24 +0000

    Add files via upload

---
+++ jasa.js
@@ -0,0 +1,54 @@
+console.log
+let nome = "Rodrigo";
+let idade = 18;
+
+let idadex7 = 126;
+
+let amigos = ["marcio", "jessica", "angelo"];
+let numbers = [1, 2, 3, 4, 5];
+console.log(amigos[0]);
+// marcio
+
+let amigo1 = {
+  nome: "marcio",
+  idade: 19,
+  estuda: false
+};
+console.log(amigos.nome);
+// marcio
+
+let amigo2 = {
+  nome: "jessica",
+  idade: 18,
+  estuda: true
+};
+console.log(amigos.nome);
+// jessica
+
+let amigo3 = {
+  nome: "angelo",
+  idade: 22,
+  estuda: false
+};
+console.log(amigos.nome);
+// angelo
+
+let amigos2 = [
+  {
+    nome: "marcio",
+  idade: 19,
+  estuda: false
+  },
+  {
+    nome: "jessica",
+    idade: 18,
+    estuda: true
+  },
+  {
+    nome: "angelo",
+  idade: 22,
+  estuda: false
+  }
+];
+console.log(amigos[0].nome);
+// marcio
